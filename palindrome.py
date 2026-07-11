c71caa73b72193045f572a423016d44c96d0a33d
Gosetti sowmya <gosettisowmya@gmail.com>
2026-07-11 11:30:29 +0530

    Add files via upload

---
+++ palindrome.py
@@ -0,0 +1,11 @@
+n=int(input("enter a number:"))
+pal=n
+rev=0
+while(n>0):
+    r=n%10
+    rev=rev*10+r
+    n=n//10
+if(pal==rev):
+    print("palindrome number:")
+else:
+    print("not a palindrome number")
