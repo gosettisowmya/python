3db377f92a49e43ddb624355a70fa365db21fba0
Gosetti sowmya <gosettisowmya@gmail.com>
2026-07-12 18:24:31 +0530

    Add files via upload

---
+++ larger of 3 num.py
@@ -0,0 +1,9 @@
+a=int(input("enter a first number:"))
+b=int(input("enter a second number:"))
+c=int(input("enter a third numbber:"))
+if a>=b and a>=c:
+    print("a is larger number:")
+elif b>=a and b>=c:
+    print("b is larger number:")
+else:
+    print("c is larger number:")
