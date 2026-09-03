dcca60590a95d083b6494760e43556096c68a3d7
Gosetti Sowmya <gosettisowmya@gmail.com>
2026-09-03 23:10:25 +0530

    Add files via upload

---
+++ Construct Uniform Parity Array II.py
@@ -0,0 +1,12 @@
+class Solution:
+    def uniformArray(self, nums1: list[int]) -> bool:
+        min_odd = min((x for x in nums1 if x % 2 == 1), default=None)
+
+        if min_odd is None:
+            return True
+
+        for x in nums1:
+            if x % 2 == 0 and x < min_odd:
+                return False
+
+        return True
